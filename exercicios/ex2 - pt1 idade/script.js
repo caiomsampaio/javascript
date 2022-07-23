ad70b07b871142da838bd2b2c4be1310fe710645
Caio Sampaio <107967988+caiomsampaio@users.noreply.github.com>
2022-07-23 18:44:53 -0300

    attmais1

--- exercicios/ex2 - pt1 idade/script.js
+++ exercicios/ex2 - pt1 idade/script.js
@@ -1,35 +1,46 @@
-function enviar() {
-    var msg = window.document.getElementById('res')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('ano')
+    var res = document.querySelector('div#res')
 
-    var idade = window.document.querySelector('input#ano')
-    var m1= window.document.querySelector('input#masc')
-    var f1= window.document.querySelector('input#fem')
-
-    var idadef = Number(idade.value)
-    var s = 2022 - idadef
-
-    res.innerHTML =`Detectamos um homem ou uma mulher de <strong>${s}</strong> anos`
-
-    if(s < 18 && m1) {
-        res.innerHTML = `Detectamos um homem de ${s} anos`
-        img.src= "menino.png"
-    } else if (s >=18 && m1) {
-        res.innerHTML = `Detectamos um homem de ${s} anos`
-        img.src= "hjovem.png"
-    } else if (s >=60 && m1) {
-        res.innerHTML = `Detectamos um homem de ${s} anos`
-        img.src= "idoso.png"
-    } else if (s < 18 && f1) {
-        res.innerHTML = `Detectamos uma mulher de ${s} anos`
-        img.src= "menina.png"
-    } else if (s >= 18 && f1) {
-        res.innerHTML = `Detectamos uma mulher de ${s} anos`
-        img.src= "mjovem.png"
-    } else if (s >=60 && f1) {
-        res.innerHTML = `Detectamos uma mulher de ${s} anos`
-        img.src= "idosa.png"
+    if (fano.value.lenght == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        res.innerHTML = `[ERRO]`
-    }
+        var fsex = document.getElementsByName('sexo')
+        var idade = ano - Number(fano.value)
+        var genero = ''
 
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'menino.png')
+            } else if (idade <= 18) {
+                // Jovem
+                img.setAttribute('src', 'hjovem.png')
+            } else if (idade <= 60) {
+                // Adulto
+                img.setAttribute('src', 'idoso.png')
+            } else {
+                //idoso
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >=0 && idade <10) {
+                // Criança
+            } else if (idade <21) {
+                // Jovem
+            } else if (idade < 50) {
+                // Adulto
+            } else {
+                //idoso
+            }
+        }
+        res.getElementsByClassName.textAllign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+     }
 }
