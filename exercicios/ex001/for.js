5a27b07e08b755b774b14165455855d325118ecd
Caio Sampaio <107967988+caiomsampaio@users.noreply.github.com>
2022-07-25 12:39:17 -0300

    att moduloD

---
+++ exercicios/ex001/for.js
@@ -0,0 +1,27 @@
+console.log('Vai começar...')
+for (var c= 1;c <= 4; c++) {
+    console.log(c)
+}
+console.log('FIM!')
+
+
+
+
+
+
+
+/*  MAIS ENCONTRADA
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++ // c = c +1
+}
+
+
+USANDO FOR:
+
+for (var c= 1;c <= 3; c++) {
+    console.log(c)
+}
+
+*/
