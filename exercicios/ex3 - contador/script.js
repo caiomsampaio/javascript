b94a3d3e06f2c87235ba8e83c886f650060423f4
Caio Sampaio <107967988+caiomsampaio@users.noreply.github.com>
2022-07-30 16:32:48 -0300

    attb

---
+++ exercicios/ex3 - contador/script.js
@@ -0,0 +1,13 @@
+function contar() {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    // Para testar:
+    if (ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        window.alert('Tudo OK!')
+    }
+}
