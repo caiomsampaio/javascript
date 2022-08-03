544b779b2c4b1454caa9f5cc8e0a05582f82c453
Caio Sampaio <107967988+caiomsampaio@users.noreply.github.com>
2022-08-03 18:02:21 -0300

    att ex  contar

--- exercicios/ex3 - contador/script.js
+++ exercicios/ex3 - contador/script.js
@@ -6,8 +6,30 @@ function contar() {
 
     // Para testar:
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!' // A ordem importa, exlcuindo quando houver içamento (?)
+       // window.alert('[ERRO] Faltam dados!')
     } else {
-        window.alert('Tudo OK!')
+        res.innerHTML = 'Contado:   <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0) {
+            window.alert('Passo inválido Considerando PASSO 1')
+            p = 1
+        }
+
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            // Contagem regressiva
+            for (let c = i; c>= f; c-= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
